9102dbfb12d21aba7f49f3ff6e1e0b2675c881e2
Eduardo Kasper <ehkasper@gmail.com>
2016-01-21 15:41:38 -0200

    baby steps - markov chain - 1st test

--- src/markov_elear/generator.clj
+++ src/markov_elear/generator.clj
@@ -1,2 +1,22 @@
 (ns markov-elear.generator)
 
+(require '[clojure.set :as set])
+
+(def example "And the Golden Grouse And the Pobble who")
+
+(def words (clojure.string/split example #" "))
+
+(def word-transitions (partition-all 3 1 words))
+
+(reduce (fn [r t] (merge-with set/union r
+                               (let [[a b c] t]
+                                 {[a b] (if c #{c} #{})})))
+          {}
+          word-transitions)
+
+(defn word-chain [word-transitions]
+  (reduce (fn [r t] (merge-with set/union r
+                               (let [[a b c] t]
+                                 {[a b] (if c #{c} #{})})))
+          {}
+          word-transitions))
